7b4e49b0f97b3e91b2c8e255062baf7f0a2daad2
Shafina Ardelia <130486863+Meadowiee@users.noreply.github.com>
2023-09-26 21:34:41 +0800

    Update Input.java

--- September/Input.java
+++ September/Input.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class Input {
+  public static void main (String[] args) {
+    Scanner input = new Scanner (System.in);
+    int due;
+    String task;
+    
+    System.out.println("==== My task today ====");
+    System.out.println("=======================");
 
+    // Input 
+    System.out.println("Task : ");
+    task = input.nextLine();
+    System.out.println("Due  : ");
+    due = input.nextInt();
+
+    //Output 
+    System.out.println("=======================");
+    System.out.println("So your task today is:");
+    System.out.println(task + " that due on " + due);
+  }
+}
